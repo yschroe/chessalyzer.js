f73c4d801d70979a03bf03a689d88de9320bfffc
PeterPain <yannik.schroeder@gmail.com>
2018-11-18 12:32:56 +0100

    base before branch

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ const c1 = new Chessalyzer();
 
 for (let i = 0; i < 1; i += 1) {
 	c1.startBatch(
-		'C:/Users/yanni/Documents/GitHub/archive/Chessalyzer_p5js/lichess_db_standard_rated_2013-01.pgn',
+		'C:/Users/yanni/Downloads/lichess_db_standard_rated_2013-01.pgn',
 		{
 			split: false,
 			cntGames: 100000,
