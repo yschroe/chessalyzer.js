50e423accf4576f43073b12ddb42afa6fe208f54
PeterPain <yannik.schroeder@gmail.com>
2021-11-27 16:35:01 +0100

    fixed build externals

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,30 @@
+// defines modules as external dependencies (= will not be bundled into the lib)
+const external = [
+	'fs',
+	'perf_hooks',
+	'chalk',
+	'readline',
+	'events',
+	'cluster',
+	'path',
+	'url'
+];
+
 export default [
 	{
 		input: 'src/index.js',
 		output: {
 			file: 'lib/chessalyzer.js',
 			format: 'esm'
-		}
+		},
+		external
 	},
 	{
 		input: 'src/core/Processor.worker.js',
 		output: {
 			file: 'lib/Processor.worker.js',
 			format: 'esm'
-		}
+		},
+		external
 	}
 ];
