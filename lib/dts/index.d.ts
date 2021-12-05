5493e9f8a2a7f3678bd017e3f6ecd66a33af04d9
Yannik Schröder <yannik.schroeder@gmail.com>
2021-12-05 09:31:37 +0100

    removed dts folder

--- lib/dts/index.d.ts
+++
@@ -1,4 +0,0 @@
-import Chessalyzer from './core/Chessalyzer.js';
-import Tracker from './tracker/Tracker.js';
-import Heatmap from './heatmaps/Heatmap.js';
-export { Chessalyzer, Tracker, Heatmap };
