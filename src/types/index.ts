3bf4af3dfe5401bcec0439a9522ab8d2e1a547fd
Yannik Schröder <5794325+YSchroe@users.noreply.github.com>
2023-05-30 17:20:57 +0200

    cleanup

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,6 @@
+type PawnToken = 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h';
 export type PieceToken = 'R' | 'N' | 'B' | 'Q' | 'K';
 
-export type PawnToken = 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h';
 export type FileLetter = PawnToken;
 export type Token = PieceToken | PawnToken | 'O';
 export type PlayerColor = 'b' | 'w';
