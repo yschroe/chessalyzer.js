3845fe5abc0cc934a38108ec9bee3571757b00fc
Yannik Schröder <5794325+yschroe@users.noreply.github.com>
2025-01-26 08:26:58 +0100

    add dts

--- scripts/create-bundle.ts
+++ scripts/create-bundle.ts
@@ -1,8 +1,24 @@
-await Bun.build({
+import dts from 'bun-plugin-dts';
+
+// Remove lib folder
+await Bun.$`rm -rf lib`;
+
+// Create a bundle
+const result = await Bun.build({
 	entrypoints: ['./src/chessalyzer.ts', './src/core/ChessWorker.ts'],
 	outdir: './lib/',
 	target: 'node',
 	splitting: true,
 	naming: '[name].[ext]',
-	minify: true
+	minify: true,
+	plugins: [dts()]
 });
+
+if (!result.success) {
+	for (const log of result.logs) {
+		console.error(log);
+	}
+}
+
+// Remove lib/core folder containing only an empty d.ts file
+await Bun.$`rm -rf lib/core`;
