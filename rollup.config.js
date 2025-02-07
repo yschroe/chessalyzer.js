995117c4a565da7869ea9bed91055c1a09c57eb6
Yannik Schröder <5794325+yschroe@users.noreply.github.com>
2025-02-07 17:45:33 +0100

    Update ESLint configuration to use new TypeScript ESLint setup

--- rollup.config.js
+++
@@ -1,9 +0,0 @@
-import dts from 'rollup-plugin-dts';
-
-export default [
-	{
-		input: 'lib/dts/chessalyzer.d.ts',
-		output: [{ file: 'lib/chessalyzer.d.ts', format: 'esm' }],
-		plugins: [dts()]
-	}
-];
