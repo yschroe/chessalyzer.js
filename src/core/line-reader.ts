2e7330535db79782b9147ef6f8bd96a5cc77fc6d
Yannik Schröder <5794325+YSchroe@users.noreply.github.com>
2025-11-07 21:02:14 +0100

    Refactor line reading logic and update import paths
    - Enhanced the readLinesFast function to improve line reading efficiency by managing a cache and handling incomplete lines.
    - Updated import statements in test files to use a direct path for the chessalyzer module, ensuring consistency across the codebase.

--- src/core/line-reader.ts
+++ src/core/line-reader.ts
@@ -2,27 +2,35 @@ import { createReadStream } from 'node:fs';
 
 export function readLinesFast(file: string): AsyncIterable<string> {
 	const rs = createReadStream(file, { encoding: 'utf-8' });
-	const iterator = rs[Symbol.asyncIterator]();
+	const iterator: AsyncIterator<string, string> = rs[Symbol.asyncIterator]();
 
-	let cache: string[] = [];
+	const cache: string[] = [];
 	let lineBreak = false;
 
 	return {
 		[Symbol.asyncIterator]: () => ({
 			next: async () => {
+				// If the cache is less than 2 lines, read in more lines
 				if (cache.length < 2) {
+					// Read in chunk of size highWaterMark (default: 64 * 1024 bytes)
 					const { value, done } = await iterator.next();
+
+					// If the iterator is not done, split the chunk into lines
 					if (!done) {
 						const lines = value.replace(/\r/g, '').split('\n');
+						// If the cache is not empty and the line break flag is not set,
+						// it means the last line of the previous chunk was not a full line.
+						// Append the first line of the new chunk to complete the line.
 						if (cache.length && !lineBreak) {
 							cache[0] += lines.shift();
-							cache.push.apply(cache, lines);
-						} else {
-							cache = lines;
 						}
-						lineBreak = value[value.length - 1] === '\n';
+						cache.push(...lines);
+
+						// Check if chunk ended with a line break
+						lineBreak = value.at(-1) === '\n';
 					}
 				}
+				// If the cache has data, return the first line
 				if (cache.length) {
 					return { value: cache.shift(), done: false };
 				}
