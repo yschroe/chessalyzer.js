88ff1b6219943fad8340f1bb5666ace711bfc5d5
Yannik Schröder <5794325+YSchroe@users.noreply.github.com>
2025-11-07 21:31:27 +0100

    Refactor readLinesFast function for improved readability and structure
    - Simplified the implementation of the readLinesFast function by extracting the line reading logic into a separate async function.
    - Enhanced code clarity by reducing nested structures and improving variable management within the function.

--- src/core/line-reader.ts
+++ src/core/line-reader.ts
@@ -1,41 +1,42 @@
 import { createReadStream } from 'node:fs';
 
-export function readLinesFast(file: string): AsyncIterable<string> {
+export function readLinesFast(file: string) {
 	const rs = createReadStream(file, { encoding: 'utf-8' });
 	const iterator: AsyncIterator<string, string> = rs[Symbol.asyncIterator]();
 
 	const cache: string[] = [];
 	let lineBreak = false;
 
+	/** Returns the next line from the file. */
+	const next = async () => {
+		// If the cache is less than 2 lines, read in more lines
+		if (cache.length < 2) {
+			// Read in chunk of size highWaterMark (default: 64 * 1024 bytes)
+			const { value, done } = await iterator.next();
+
+			// If the iterator is not done, split the chunk into lines
+			if (!done) {
+				const lines = value.replace(/\r/g, '').split('\n');
+				// If the cache is not empty and the line break flag is not set,
+				// it means the last line of the previous chunk was not a full line.
+				// Append the first line of the new chunk to complete the line.
+				if (cache.length && !lineBreak) cache[0] += lines.shift();
+				cache.push(...lines);
+
+				// Check if chunk ended with a line break
+				lineBreak = value.at(-1) === '\n';
+			}
+		}
+		// If the cache has data, return the first line
+		if (cache.length) {
+			return { value: cache.shift(), done: false };
+		}
+		return { value: undefined, done: true };
+	};
+
 	return {
 		[Symbol.asyncIterator]: () => ({
-			next: async () => {
-				// If the cache is less than 2 lines, read in more lines
-				if (cache.length < 2) {
-					// Read in chunk of size highWaterMark (default: 64 * 1024 bytes)
-					const { value, done } = await iterator.next();
-
-					// If the iterator is not done, split the chunk into lines
-					if (!done) {
-						const lines = value.replace(/\r/g, '').split('\n');
-						// If the cache is not empty and the line break flag is not set,
-						// it means the last line of the previous chunk was not a full line.
-						// Append the first line of the new chunk to complete the line.
-						if (cache.length && !lineBreak) {
-							cache[0] += lines.shift();
-						}
-						cache.push(...lines);
-
-						// Check if chunk ended with a line break
-						lineBreak = value.at(-1) === '\n';
-					}
-				}
-				// If the cache has data, return the first line
-				if (cache.length) {
-					return { value: cache.shift(), done: false };
-				}
-				return { value: undefined, done: true };
-			}
+			next
 		})
 	};
 }
