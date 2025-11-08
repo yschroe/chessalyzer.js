d2beef1753569aa8b662feb294e25a017acd154a
Yannik Schröder <5794325+YSchroe@users.noreply.github.com>
2025-11-08 08:01:25 +0100

    ok

--- manual-tests/test_release.js
+++ manual-tests/test_release.js
@@ -1,19 +1,4 @@
-import {
-	Chessalyzer,
-	GameTracker,
-	PieceTracker,
-	TileTracker
-} from 'chessalyzer.js';
-import CustomGameTracker from './CustomGameTracker.js';
-
-const a = new GameTracker();
-const b = new PieceTracker();
-const c = new TileTracker();
-const d = new CustomGameTracker();
-
-// a.cfg.profilingActive = true;
-// b.cfg.profilingActive = true;
-c.cfg.profilingActive = true;
+import { Chessalyzer } from 'chessalyzer.js';
 
 console.time('Time elapsed');
 const header = await Chessalyzer.analyzePGN(
