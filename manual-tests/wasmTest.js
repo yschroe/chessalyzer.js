75cbc223f479041ef68b979f2b17edd2b57ce76b
Yannik Schröder <5794325+yschroe@users.noreply.github.com>
2026-07-25 17:51:58 +0200

    Revert "refactor/perf: Improve performance and refactor structure (#7)" (#8)
    This reverts commit c8473cd48df6d59d8b43051bf323499461922486.

--- manual-tests/wasmTest.js
+++
@@ -1,4 +0,0 @@
-import { Board } from '#bitboard';
-
-const board = new Board();
-console.log('Board loaded, find_attacker:', typeof board.find_attacker);
--- src/core/chess-worker.ts
+++ src/core/chess-worker.ts
@@ -21,10 +21,8 @@ TrackerList[GameTracker.name] = GameTracker;
 
 parentPort.on('message', (msg: WorkerTaskData) => {
     void (async () => {
-        const { trackerData, games, idxConfig } = msg;
-
-        // Import custom trackers
-        for (const tracker of trackerData.filter((val) => val.path)) {
+        // import custom trackers
+        for (const tracker of msg.trackerData.filter((val) => val.path)) {
             if (!(tracker.name in TrackerList)) {
                 const customTracker = await import(tracker.path);
                 TrackerList[tracker.name] = customTracker.default
@@ -33,14 +31,13 @@ parentPort.on('message', (msg: WorkerTaskData) => {
             }
         }
 
-        // Select needed trackers
+        // select needed trackers
         const cfg: GameProcessorAnalysisConfig = {
             trackers: { move: [], game: [] },
             processedMoves: 0,
             processedGames: 0,
         };
-
-        for (const tracker of trackerData) {
+        for (const tracker of msg.trackerData) {
             const currentTracker: BaseTracker = new TrackerList[tracker.name]();
             currentTracker.cfg = tracker.cfg;
 
@@ -52,14 +49,14 @@ parentPort.on('message', (msg: WorkerTaskData) => {
         }
 
         // analyze each game
-        for (const game of games) gameParser.processGame(game, cfg);
+        for (const game of msg.games) gameParser.processGame(game, cfg);
 
         const result: WorkerMessage = {
             cntMoves: cfg.processedMoves,
             cntGames: cfg.processedGames,
             gameTrackers: cfg.trackers.game,
             moveTrackers: cfg.trackers.move,
-            idxConfig,
+            idxConfig: msg.idxConfig,
         };
 
         // send result of batch to master
@@ -70,6 +67,6 @@ parentPort.on('message', (msg: WorkerTaskData) => {
 // handle errors
 // since above code runs inside a promise, simply catching and rethrowing
 // causes a ERR_UNHANDLED_REJECTION error
-process.on('unhandledRejection', (e: any) => {
-    throw e.message;
+process.on('unhandledRejection', (e: Error) => {
+    throw e;
 });
