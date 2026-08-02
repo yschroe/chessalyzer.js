01f43849ef3b2571609edf165f85ec0c62401221
Yannik Schröder <5794325+YSchroe@users.noreply.github.com>
2026-08-02 21:10:57 +0200

    chore: update bump.config.ts to commit all pending changes

--- bump.config.ts
+++ bump.config.ts
@@ -5,4 +5,5 @@ export default defineConfig({
     commit: 'chore: release {version}',
     tag: 'v{version}',
     push: false,
+    all: true, // Commit all pending changes, not just the files listed in `files`.
 });
